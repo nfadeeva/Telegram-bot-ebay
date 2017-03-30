6cacdc610e09686a210bfdea8e99f6103929b942
nfadeeva <fadeevanastasiia@gmail.com>
2017-03-30 19:41:34 +0300

    Add creating xml-request function

--- api.py
+++ api.py
@@ -2,29 +2,47 @@ import requests
 import config
 from xml.dom import minidom
 from io import StringIO, BytesIO
+import xml.etree.ElementTree as ET
 
 
-def request(keywords):
-    headers = {'X-EBAY-SOA-SERVICE-NAME': 'FindingService',
-               'X-EBAY-SOA-OPERATION-NAME': 'findItemsByKeywords',
-               'X-EBAY-SOA-SECURITY-APPNAME': config.Key,
-               'Content-Type': 'text/xml'}
-
-    xml = """<?xml version="1.0" encoding="UTF-8"?>
-    <findItemsByKeywordsRequest xmlns="http://www.ebay.com/marketplace/search/v1/services">
-      <keywords>""" + keywords + """</keywords>
-      <outputSelector>SellerInfo</outputSelector>
-      <paginationInput>
-        <entriesPerPage>20</entriesPerPage>
-      </paginationInput>
-    </findItemsByKeywordsRequest>"""
-
-    s = requests.post('http://svcs.ebay.com/services/search/FindingService/v1', data=xml, headers=headers)
-    return BytesIO(s.content)
+def findItemsByKeywords(
+        keywords, outputSelector=None,
+        paginationInput=None,
+        sortOrder=None, itemFilter=None):
 
 
-xmldoc = minidom.parse(request('pen'))
+    root = ET.Element("findItemsByKeywords",
+                         xmlns="http://www.ebay.com/marketplace/search/v1/services")
 
+    keywords_elem = ET.SubElement(root, "keywords")
+    keywords_elem.text = keywords
+
+    # outputSelector is a list
+    if outputSelector:
+        for item in outputSelector:
+            outputSelector_elem = ET.SubElement(root, "outputSelector")
+            outputSelector_elem.text = item
+
+    # paginationInput is a dict
+    if paginationInput:
+        paginationInput_elem = ET.SubElement(root, "paginationInput")
+        for key in paginationInput:
+            key_elem = ET.SubElement(paginationInput_elem, key)
+            key_elem.text = paginationInput[key]
+
+    # itemFilter is a list of dicts
+    if itemFilter:
+        for item in itemFilter:
+            itemFilter_elem = ET.SubElement(root, "itemFilter")
+            for key in item:
+                key_elem = ET.SubElement(itemFilter_elem, key)
+                key_elem.text = item[key]
+
+    # sortOrder
+    if sortOrder:
+        sortOrder_elem = ET.SubElement(root, "sortOrder")
+        sortOrder_elem.text = sortOrder
+    return ET.tostring(root)
 
 def parse_xml(xmldoc, tag):
     """Get the xmldoc and tag and return list of elements"""
@@ -42,3 +60,17 @@ def parse_sellerInfo(xmldoc):
             element = element.nextSibling
             print(element.localName, element.firstChild.nodeValue)
         print()
+def request(keywords):
+    headers = {'X-EBAY-SOA-SERVICE-NAME': 'FindingService',
+               'X-EBAY-SOA-OPERATION-NAME': 'findItemsByKeywords',
+               'X-EBAY-SOA-SECURITY-APPNAME': config.Key,
+               'Content-Type': 'text/xml'}
+
+    xml = findItemsByKeywords('pen',outputSelector=['SellerInfo'],paginationInput={'entriesPerPage':'20', 'pageNumber': '1'})
+
+    s = requests.post('http://svcs.ebay.com/services/search/FindingService/v1', data=xml, headers=headers)
+    return BytesIO(s.content)
+
+
+xmldoc = minidom.parse(request('pen'))
+parse_sellerInfo(xmldoc)
