d5b41c83a63a261266c769c7db54d64fa771769a
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-06 18:35:44 +0300

    Fix dialog with user in Bot.py

--- Bot.py
+++ Bot.py
@@ -8,31 +8,64 @@ bot = telebot.TeleBot(config.token)
 sort_orders = ['BestMatch', 'PricePlusShippingLowest', 'StartTimeNewest', 'EndTimeSoonest', 'None']
 sellers_sort_orders = ['Rating']
 level = 0
-helper = EbayApiHelper()
+sort = None
+keywords = None
+request_dict = {}
+
+class Request:
+    def __init__(self):
+        self.keywords = None
+        self.sort = None
+        self.num = None
 
 @bot.message_handler(commands=['start'])
-def messageHandler(message):
+def start(message):
     bot.send_message(message.chat.id, "What are you searching for?")
+    bot.register_next_step_handler(message, process_keywords)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def messageHandler(message):
-    global level
-    level += 1
-    if (level == 1):
-        helper.keywords = message
-        bot.send_message(message.chat.id, reply_markup=generate_markup(sort_orders),
+def process_keywords(message):
+    try:
+        chat_id = message.chat.id
+        request = Request()
+        request_dict[chat_id] = request
+        request.keywords = message.text
+        bot.reply_to(message, reply_markup=generate_markup(sort_orders),
                          text="Please, choose sort order")
-    if (level == 2):
-        helper.sort = message
-        bot.send_message(message.chat.id, reply_markup=generate_markup(sellers_sort_orders),
-                         text="..Sellers")
+        bot.register_next_step_handler(message, process_sort)
+    except Exception:
+        bot.reply_to(message, 'error, sorry')
 
-    if (level == 3):
-        bot.send_message(message.chat.id,
+def process_sort(message):
+    try:
+        chat_id = message.chat.id
+        request = request_dict[chat_id]
+        request.sort = message.text
+        bot.reply_to(message, reply_markup=generate_markup(sellers_sort_orders),
+                         text="..Sellers")
+        bot.register_next_step_handler(message, process_sellers_sort)
+    except Exception:
+        bot.reply_to(message, 'error, sorry')
+
+def process_sellers_sort(message):
+    try:
+        chat_id = message.chat.id
+        request = request_dict[chat_id]
+        bot.reply_to(message,
                          text="How many items do you want to see")
-    if (level == 4):
+        bot.register_next_step_handler(message, process_num)
+    except Exception:
+        bot.reply_to(message, 'error, sorry')
+
+def process_num(message):
+    try:
+        chat_id = message.chat.id
+        request = request_dict[chat_id]
+        bot.reply_to(message, text=request.keywords)
+        helper = EbayApiHelper(keywords, sort)
         helper.request()
-        pass
+    except Exception:
+        bot.reply_to(message, 'error, sorry')
+
 
 def generate_markup(items):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
