266ae1854991667dd368abbb07ab6d983962e932
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-19 23:22:55 +0300

    Add decorator to wrap errors and /'cmd' handling

--- BotClass.py
+++ BotClass.py
@@ -1,16 +1,17 @@
 import telebot
 import config
 import random
-import collections
 from telebot import types
 from xml.dom import minidom
 from EbayApiHelper import EbayApiHelper
 from ResponseParser import ResponseParser
 import time
+import functools
 
 sort_orders = ['BestMatch', 'PricePlusShippingLowest', 'StartTimeNewest', 'EndTimeSoonest', 'None']
 sellers_sort_orders = ['Rating', 'FeedbackScore','None']
 settings = ['Keywords', 'Sort', 'Sellers', 'Solds', 'Rating']
+changes = ['Get results', 'Another', 'Accept']
 markups = {'Sort': sort_orders, 'Sellers': sellers_sort_orders}
 pages = 100
 
@@ -35,142 +36,129 @@ class Bot:
                              text="What settings do you want to change?")
         Bot.bot.register_next_step_handler(message, Bot.process_settings)
 
-    def process_settings(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict.get(chat_id)
-                if not request:
-                    request = Bunch(keywords=None, sort=None, sellers=None, solds=None, rating=None, progress=0,
-                                    change=None)
-                    Bot.request_dict[chat_id] = request
-                request.change = message.text
-                #CHANGE TEXT!
-                Bot.bot.send_message(message.chat.id, reply_markup=generate_markup(markups.get(message.text)),
-                                     text="Please, enter value")
-                Bot.bot.register_next_step_handler(message, Bot.process_changes)
+    def handler(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                message = args[0]
+                if (message.text == '/start'):
+                    return
+                return func(*args, **kwargs)
+            except Exception as e:
+                Bot.bot.reply_to(message, e)
+        return wrapper
 
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+    @handler
+    def process_settings(message):
+        chat_id = message.chat.id
+        request = Bot.request_dict.get(chat_id)
+        if not request:
+            request = Bunch(keywords=None, sort=None, sellers=None, solds=None, rating=None, progress=0,
+                            change=None)
+            Bot.request_dict[chat_id] = request
+        request.change = message.text
+        #CHANGE TEXT!
+        Bot.bot.send_message(message.chat.id, reply_markup=generate_markup(markups.get(message.text)),
+                             text="Please, enter value")
+        Bot.bot.register_next_step_handler(message, Bot.process_changes)
 
     def process_changes(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                print(request.change.lower())
-                setattr(request, request.change.lower(), message.text)
-                print(getattr(request,request.change.lower()))
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        print(request.change.lower())
+        setattr(request, request.change.lower(), message.text)
+        print(getattr(request,request.change.lower()))
+        Bot.bot.send_message(message.chat.id, reply_markup=generate_markup(changes),
+                             text="What do you want to do?")
+        Bot.bot.register_next_step_handler(message, Bot.process_next_changes)
+
+    def process_next_changes(message):
+        text = message.text
+        chat_id = message.chat.id
+        if (text=='Another'):
+            Bot.settings(message)
+        elif (text == 'Get results'):
+            request = Bot.request_dict[chat_id]
+            helper = EbayApiHelper(request.keywords, request)
+            futures = helper.futures(100)
+            xmldocs = []
+            for i in futures:
+                xmldoc = minidom.parse(i.result())
+                xmldocs.append(xmldoc)
+            parser = ResponseParser(xmldocs, request.sellers, request.rating, request.solds)
+            items = list(map(lambda x: x[0], parser.parse_request()))
+            for item in items[:request.num]:
+                Bot.bot.send_message(message.chat.id, item)
 
     def process_keywords(message):
-        try:
-            if(message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bunch(keywords=None, sort=None, sellers=None, solds=None,rating = None,progress=0, change=None)
-                Bot.request_dict[chat_id] = request
-                request.keywords = message.text
-                Bot.bot.reply_to(message, reply_markup=generate_markup(sort_orders),
-                                 text="Please, choose the sort order")
-                Bot.bot.register_next_step_handler(message, Bot.process_sort)
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bunch(keywords=None, sort=None, sellers=None, solds=None, rating=None, progress=0, change=None)
+        Bot.request_dict[chat_id] = request
+        request.keywords = message.text
+        Bot.bot.reply_to(message, reply_markup=generate_markup(sort_orders),
+                         text="Please, choose the sort order")
+        Bot.bot.register_next_step_handler(message, Bot.process_sort)
 
     def process_sort(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                request.sort = message.text
-                Bot.bot.reply_to(message, reply_markup=generate_markup(sellers_sort_orders),
-                                 text="Please, choose the sort order(sellers)")
-                Bot.bot.register_next_step_handler(message, Bot.process_sellers_sort)
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        request.sort = message.text
+        Bot.bot.reply_to(message, reply_markup=generate_markup(sellers_sort_orders),
+                         text="Please, choose the sort order(sellers)")
+        Bot.bot.register_next_step_handler(message, Bot.process_sellers_sort)
 
     def process_sellers_sort(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                request.sellers = message.text
-                Bot.bot.reply_to(message,
-                                 text="How high should be seller's score? Please, enter the number from 0 to 100")
-                Bot.bot.register_next_step_handler(message, Bot.process_sellers_rating)
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        request.sellers = message.text
+        Bot.bot.reply_to(message,
+                         text="How high should be seller's score? Please, enter the number from 0 to 100")
+        Bot.bot.register_next_step_handler(message, Bot.process_sellers_rating)
 
     def process_sellers_rating(message):
-        try:
-            if(message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                request.rating = message.text
-                Bot.bot.reply_to(message,
-                                 text="How high should be number of seller's sold items")
-                Bot.bot.register_next_step_handler(message, Bot.process_sellers_solds)
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        request.rating = message.text
+        Bot.bot.reply_to(message,
+                         text="How high should be number of seller's sold items")
+        Bot.bot.register_next_step_handler(message, Bot.process_sellers_solds)
 
     def process_sellers_solds(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                request.solds = message.text
-                Bot.bot.reply_to(message,
-                                 text="How many items do you want to see?")
-                Bot.bot.register_next_step_handler(message, Bot.process_num)
-        except Exception as e:
-            Bot.bot.reply_to(message, e)
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        request.solds = message.text
+        Bot.bot.reply_to(message,
+                         text="How many items do you want to see?")
+        Bot.bot.register_next_step_handler(message, Bot.process_num)
 
     def process_num(message):
-        try:
-            if (message.text == '/start'):
-                return
-            else:
-                num = message.text
-                if not num.isdigit():
-                    message = bot.reply_to(message,
-                                       "Num of items should be a number. How many items do you want to see?")
-                    bot.register_next_step_handler(message, Bot.process_num)
-                    return
-                chat_id = message.chat.id
-                request = Bot.request_dict[chat_id]
-                request.num = (int)(num)
-                helper = EbayApiHelper(request.keywords, request)
-                futures = helper.futures(100)
-                xmldocs = []
-                for i in futures:
-                    xmldoc =  minidom.parse(i.result())
-                    xmldocs.append(xmldoc)
-                parser = ResponseParser(xmldocs, request.sellers, request.rating, request.solds)
-                items = list(map(lambda x : x[0],parser.parse_request()))
-                for item in items[:request.num]:
-                    Bot.bot.send_message(message.chat.id, item)
-        except Exception as e:
-            print(e)
-            Bot.bot.reply_to(message, e)
+        num = message.text
+        if not num.isdigit():
+            message = bot.reply_to(message,
+                                   "Num of items should be a number. How many items do you want to see?")
+            bot.register_next_step_handler(message, Bot.process_num)
+            return
+        chat_id = message.chat.id
+        request = Bot.request_dict[chat_id]
+        request.num = (int)(num)
+        helper = EbayApiHelper(request.keywords, request)
+        futures = helper.futures(100)
+        xmldocs = []
+        start = time.time()
+        for i in futures:
+            xmldoc = minidom.parse(i.result())
+            xmldocs.append(xmldoc)
+        end = time.time()
+        print(end - start)
+        parser = ResponseParser(xmldocs, request.sellers, request.rating, request.solds)
+        items = list(map(lambda x: x[0], parser.parse_request()))
+        for item in items[:request.num]:
+            Bot.bot.send_message(message.chat.id, item)
 
-    @bot.message_handler(commands=['prog'])
-    def progr(message):
-        request = Bot.request_dict[message.chat.id]
-        Bot.bot.send_message(message.chat.id, request.progress)
+        # @bot.message_handler(commands=['prog'])
+        # def progr(message):
+        #     request = Bot.request_dict[message.chat.id]
+        #     Bot.bot.send_message(message.chat.id, request.progress)
 
 
 def generate_markup(items):
