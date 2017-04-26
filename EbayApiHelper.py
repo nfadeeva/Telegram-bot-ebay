93e201c291f1d4aba1ca2a7d9476f174517b219f
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-26 23:49:58 +0300

    Fix progress reload

--- EbayApiHelper.py
+++ EbayApiHelper.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import config
 import concurrent.futures
 import time
+from collections import OrderedDict
 class EbayApiHelper(object):
     """
     This class is for making xml request and get xml response
@@ -17,6 +18,8 @@ class EbayApiHelper(object):
                    'Content-Type': 'text/xml'}
         self.__pagination_input = {'entriesPerPage': '100', 'pageNumber': page}
         self.__output_selector = ['SellerInfo']
+        self.itemFilter = OrderedDict([('name', 'listingType'), ('value','FixedPrice')])
+
         self.__keywords = keywords
         self.__sort = sort
         self.__request = request
@@ -31,6 +34,12 @@ class EbayApiHelper(object):
 
         keywords_elem = ET.SubElement(root, "keywords")
         keywords_elem.text = keywords
+
+        if self.itemFilter:
+            itemFilter_elem = ET.SubElement(root, "itemFilter")
+            for key in self.itemFilter:
+                key_elem = ET.SubElement(itemFilter_elem, key)
+                key_elem.text = self.itemFilter[key]
 
         # outputSelector is a list
         if self.__output_selector:
@@ -52,12 +61,13 @@ class EbayApiHelper(object):
             sort_elem.text = self.__sort
         return ET.tostring(root).decode("utf-8")
 
-    def request(self, page):
+    def request(self, page, pages):
         s = requests.post('http://svcs.ebay.com/services/search/FindingService/v1', data=self.createXml(page), headers=self.__headers)
-        self.__request.progress+=1;
+        self.__request.progress+=int(1/pages);
         return BytesIO(s.content)
 
     def futures(self,pages):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
-            futures = {executor.submit(self.request, page): page for page in range(1, pages)}
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            futures = {executor.submit(self.request, page, pages): page for page in range(1, pages)}
+        print("fin")
         return futures
